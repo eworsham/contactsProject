35956f33b9e3ea4f65734a683c0be4ba4e9c7f13
eworsham <eworsham64@gmail.com>
2024-11-09 16:45:58 -0500

    Added simple hello world route

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use('/', require('./routes'))
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
