e8bb38d41a3fadb05d66bebceb921d8caafefdcc
eworsham <eworsham64@gmail.com>
2024-11-14 10:36:32 -0500

    Added endpoints for POST, PUT, and DELETE

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -18,7 +18,57 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createContact = async (req, res) => {
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb.getDatabase().db().collection('contacts').insertOne(contact)
+  if (result.acknowledged) {
+    res.status(201).json(
+      {
+        id: result.insertedId
+      }
+    )
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while creating the contact')
+  }
+}
+
+const updateContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id)
+  const contact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  }
+  const result = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: contactId }, contact)
+  if (result.modifiedCount > 0) {
+    res.status(204).send()
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while updating the contact')
+  }
+}
+
+const deleteContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id)
+  const result = await mongodb.getDatabase().db().collection('contacts').deleteOne({ _id: contactId })
+  if (result.deletedCount > 0) {
+    res.status(204).send()
+  } else {
+    res.status(500).json(result.error || 'Some error occurred while deleting the contact')
+  }
+}
+
 module.exports = {
   getAll,
-  getSingle
+  getSingle,
+  createContact,
+  updateContact,
+  deleteContact
 };
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 const mongodb = require('./data/database');
 
 const PORT = process.env.PORT || 3000;
 
+app.use(bodyParser.json())
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
